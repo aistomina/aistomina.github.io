31ef8a2a50bcc60bd37e151c262571dbfc37a179
Anna Istomina <istomina.anna@gmail.com>
2017-11-24 09:51:04 -0800

    name -> logo transition

--- main.js
+++ main.js
@@ -3,6 +3,9 @@ $(function() {
   var links = $('header > #links > a');
   var scrollHint = $('.scroll-hint');
   var headerColor = '51,51,50';
+  var logo = $('#logo');
+  var annaIstomina = $('.anna-istomina');
+  var annaIstominaLink = $('.anna-istomina > a');
 
   function recomputeHeader() {
     var top = $(window).scrollTop();
@@ -19,26 +22,40 @@ $(function() {
       scrollHint.fadeIn();
     }
 
+    if (top >= offsetBeforeHeaderFx && top < offsetAfterHeaderFx) {
+      var opacity = (top - offsetBeforeHeaderFx) / (offsetAfterHeaderFx - offsetBeforeHeaderFx);
+    }
+
     if (top < offsetBeforeHeaderFx) {
       header.css('height', `${maxHeaderHeight}px`);
       header.css('background-color', '');
-      header.css('color', '');
+      annaIstominaLink.css('color', '');
 
       links.css('color', '');
       links.css('border-color', '');
+      logo.css('opacity', `0`);
+      annaIstomina.css('margin-left', `0`);
     } else if (top >= offsetBeforeHeaderFx && top < offsetAfterHeaderFx) {
       var opacity = (top - offsetBeforeHeaderFx) / (offsetAfterHeaderFx - offsetBeforeHeaderFx);
       var colorStrength = (255 * opacity) | 0;
       header.css('background-color', `rgba(${headerColor}, ${opacity})`);
       header.css('height', `${opacity * minHeaderHeight + (1.0 - opacity) * maxHeaderHeight}px`);
-      header.css('color', `rgb(${colorStrength}, ${colorStrength}, ${colorStrength})`);
+
+      logo.css('opacity', `${opacity}`);
+      logo.css('left', `${opacity * 40}px`);
+      annaIstomina.css('margin-left', `${opacity * 40}px`);
+      annaIstominaLink.css('color', `rgb(${colorStrength}, ${colorStrength}, ${colorStrength})`);
 
       links.css('color', `rgb(${colorStrength}, ${colorStrength}, ${colorStrength})`);
       links.css('border-color', `rgb(${colorStrength}, ${colorStrength}, ${colorStrength})`);
     } else {
       header.css('background-color', `rgba(${headerColor}, 1.0)`);
       header.css('height', `${minHeaderHeight}px`);
-      header.css('color', `rgba(255, 255, 255, 1.0)`);
+
+      logo.css('opacity', `1.0`);
+      logo.css('left', `40px`);
+      annaIstomina.css('margin-left', `40px`);
+      annaIstominaLink.css('color', `rgba(255, 255, 255, 1.0)`);
 
       links.css('color', `rgba(255, 255, 255, 1.0)`);
       links.css('border-color', `rgba(255, 255, 255, 1.0)`);
@@ -50,7 +67,9 @@ $(function() {
   $(window).resize(recomputeHeader);
 
   $('.accordion').accordion({
-    heightStyle: "content"
+    heightStyle: "content",
+    collapsible: true,
+    active: false,
   });
 });
 
