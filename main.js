6961abb463788284b5dc96eedd867ae7f8e375fb
Anna Istomina <istomina.anna@gmail.com>
2017-11-23 22:54:50 -0800

    tweaks galore

--- main.js
+++ main.js
@@ -2,13 +2,14 @@ $(function() {
   var header = $('header');
   var links = $('header > #links > a');
   var scrollHint = $('.scroll-hint');
-  var offsetBeforeHeaderFx = 50;
-  var offsetAfterHeaderFx = 350;
-  var maxHeaderHeight = 200;
   var headerColor = '51,51,50';
 
-  $(window).scroll(function() {
+  function recomputeHeader() {
     var top = $(window).scrollTop();
+    var minHeaderHeight = 100;
+    var maxHeaderHeight = Math.max($(document).width() / 12, minHeaderHeight);
+    var offsetBeforeHeaderFx = 50;
+    var offsetAfterHeaderFx = offsetBeforeHeaderFx * 7;
 
     if (top > 0) {
       header.addClass('header-with-shadow');
@@ -19,7 +20,9 @@ $(function() {
     }
 
     if (top < offsetBeforeHeaderFx) {
+      header.css('height', `${maxHeaderHeight}px`);
       header.css('background-color', '');
+      header.css('color', '');
 
       links.css('color', '');
       links.css('border-color', '');
@@ -27,20 +30,30 @@ $(function() {
       var opacity = (top - offsetBeforeHeaderFx) / (offsetAfterHeaderFx - offsetBeforeHeaderFx);
       var colorStrength = (255 * opacity) | 0;
       header.css('background-color', `rgba(${headerColor}, ${opacity})`);
-      header.css('height', `${maxHeaderHeight * (1.0 - opacity / 2.0)}px`);
+      header.css('height', `${opacity * minHeaderHeight + (1.0 - opacity) * maxHeaderHeight}px`);
+      header.css('color', `rgb(${colorStrength}, ${colorStrength}, ${colorStrength})`);
 
       links.css('color', `rgb(${colorStrength}, ${colorStrength}, ${colorStrength})`);
       links.css('border-color', `rgb(${colorStrength}, ${colorStrength}, ${colorStrength})`);
     } else {
       header.css('background-color', `rgba(${headerColor}, 1.0)`);
-      header.css('height', `${maxHeaderHeight / 2.0}px`);
+      header.css('height', `${minHeaderHeight}px`);
+      header.css('color', `rgba(255, 255, 255, 1.0)`);
 
       links.css('color', `rgba(255, 255, 255, 1.0)`);
       links.css('border-color', `rgba(255, 255, 255, 1.0)`);
     }
-  });
+  }
+
+  recomputeHeader();
+  $(window).scroll(recomputeHeader);
+  $(window).resize(recomputeHeader);
 
   $('.accordion').accordion({
     heightStyle: "content"
   });
 });
+
+function scrollToContact() {
+  $("html, body").animate({ scrollTop: $(document).height() }, 1000);
+}
